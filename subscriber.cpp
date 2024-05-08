4f515431cfbc5b46f71fd2a45d76920bac71c338
Tintu Gabriel <gabriel.tintu@yahoo.com>
2024-05-08 21:36:36 +0000

    almost finished subscriber.cpp

--- subscriber.cpp
+++ subscriber.cpp
@@ -13,6 +13,66 @@
 std::ofstream fout("file_subscriber.out");
 
 #define MAX_LEN 100
+#define MAX_SOCK 2
+
+/**
+ * Send message to the server
+ * Subscribe/Unsubscribe to/from topic X
+ */
+void send_to_server(char buff[], int sockfd)
+{
+	int rc;
+	chat_packet send_message;
+	strcpy(send_message.message, buff);
+	send_message.len = strlen(send_message.message);
+	rc = send_all(sockfd, &send_message, sizeof(send_message));
+	DIE(rc < 0, "send");
+
+	std::string input(send_message.message, send_message.len);
+
+	size_t space_pos = input.find(' ');
+	
+	// Extract the first part of the message before the space
+	std::string command = input.substr(0, space_pos);
+
+	// Extract the second part of the message after the space
+	std::string topic;
+
+	if (space_pos != std::string::npos) {
+		topic = input.substr(space_pos + 1);
+	}
+
+	if (command == "subscribe")
+		std::cout << "Subscribed to topic " << topic;
+	else
+		std::cout << "Unsubscribed from topic " << topic;
+}
+
+/*
+ * Receive the message sent by UDP clients through the server
+ */
+bool recv_from_server(pollfd poll_fds[], int sockfd)
+{
+	struct tcp_message message;
+	uint16_t actual_size = 0;
+	int rc;
+
+	rc = recv(sockfd, &actual_size, sizeof(actual_size), 0);
+	DIE(rc < 0, "receiving size");
+
+	rc = recv_all(sockfd, &message, actual_size);
+
+	if (rc == 0) {
+		for (int i = 0; i < MAX_SOCK; i++) {
+			close(poll_fds[i].fd);
+		}
+		return true;
+	}
+
+	std::cout  << message.ip << ":" << message.port << " - " << message.topic << " - " << message.data_type << " - " << message.content << "\n";
+	return false;
+}
+
 
 int main(int argc, char *argv[]) {
 	setvbuf(stdout, NULL, _IONBF, BUFSIZ);
@@ -22,7 +82,7 @@ int main(int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	struct pollfd poll_fds[2];
+	pollfd poll_fds[MAX_SOCK];
 	// Parse command-line arguments
 	std::string client_id = argv[1];
 	std::string server_ip = argv[2];
@@ -37,13 +97,13 @@ int main(int argc, char *argv[]) {
 	}
 
 	// Initialize server address structure
-	sockaddr_in serverAddr;
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(port);
-	inet_pton(AF_INET, server_ip.c_str(), &serverAddr.sin_addr);
-	fout << "client id: " << client_id << "\n";
+	sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+	inet_pton(AF_INET, server_ip.c_str(), &server_addr.sin_addr);
+
 	// Connect to the server
-	rc = connect(sockfd, (struct sockaddr*) &serverAddr, sizeof(serverAddr));
+	rc = connect(sockfd, (struct sockaddr*) &server_addr, sizeof(server_addr));
 	DIE(rc < 0, "connect");
 	
 	poll_fds[0].fd = sockfd; // pt server
@@ -55,7 +115,6 @@ int main(int argc, char *argv[]) {
 	int num_sockets = 2;
 
 	struct chat_packet sent_packet;
-	struct chat_packet recv_packet;
 	std::string client_data = client_id + " " + server_ip + ":" + port_str;
 	sent_packet.len = client_data.size() + 1;
 	strcpy(sent_packet.message, client_data.c_str());
@@ -68,80 +127,23 @@ int main(int argc, char *argv[]) {
 		char buff[MAX_LEN];
 		rc = poll(poll_fds, num_sockets, -1);
 		DIE(rc < 0, "poll");
-		// if (poll_fds[0].revents & POLLIN) {
-		// 	if (recv_all(sockfd, &recv_packet, sizeof(recv_packet)) == 0) {
-		// 		close(sockfd);
-		// 		return 0;
-		// 	}
-		// }
 
-			if (poll_fds[0].revents & POLLIN) {
-					struct tcp_message message;
-					uint16_t actual_size = 0;
+		if (poll_fds[0].revents & POLLIN) {
+			bool close = recv_from_server(poll_fds, sockfd);
+			if (close)
+				break;
+		} else if (poll_fds[1].revents & POLLIN) {
+			fgets(buff, MAX_LEN - 1, stdin);
 
-					rc = recv(sockfd, &actual_size, sizeof(actual_size), 0);
-					DIE(rc < 0, "receiving size");
-
-					rc = recv_all(sockfd, &message, actual_size);
-
-					if (rc == 0) {
-						close(sockfd);
-						return 0;
-					}
-
-					std::cout  << message.ip << ":" << message.port << " - " << message.topic << " - " << message.data_type << " - " << message.content << "\n";
-			} else if (poll_fds[1].revents & POLLIN) {
-					fgets(buff, MAX_LEN - 1, stdin);
-				
-					if (strcmp(buff, "exit\n") == 0) {
-						for (int j = 0; j < num_sockets; j++) {
-							close(poll_fds[j].fd);
-						}
-						return 0;
-					} else if (strncmp(buff, "subscribe", 9) == 0) {
-						struct chat_packet send_subscribe;
-						strcpy(send_subscribe.message, buff);
-						send_subscribe.len = strlen(send_subscribe.message);
-						rc = send_all(sockfd, &send_subscribe, sizeof(send_subscribe));
-						DIE(rc < 0, "send");
-
-						std::string command(send_subscribe.message, send_subscribe.len);
-
-						size_t space_pos = command.find(' ');
-						
-						// Extract the first part of the message before the space
-						std::string sub = command.substr(0, space_pos);
-
-						// Extract the second part of the message after the space
-						std::string topic;
-						if (space_pos != std::string::npos) {
-							topic = command.substr(space_pos + 1);
-						}
-						std::cout << "Subscribed to topic " << topic;
-					} else if (strncmp(buff, "unsubscribe", 11) == 0) {
-						struct chat_packet send_unsubscribe;
-						strcpy(send_unsubscribe.message, buff);
-						send_unsubscribe.len = strlen(send_unsubscribe.message);
-						rc = send_all(sockfd, &send_unsubscribe, sizeof(send_unsubscribe));
-						DIE(rc < 0, "send");
-
-						std::string command(send_unsubscribe.message, send_unsubscribe.len);
-						size_t space_pos = command.find(' ');
-
-						// Extract the first part of the message before the space
-						std::string unsub = command.substr(0, space_pos);
-
-						// Extract the second part of the message after the space
-						std::string topic;
-						if (space_pos != std::string::npos) {
-							topic = command.substr(space_pos + 1);
-						}
-
-						std::cout << "Unsubscribed from topic " << topic;
-
-					}
+			if (strcmp(buff, "exit\n") == 0) {
+				for (int i = 0; i < num_sockets; i++) {
+					close(poll_fds[i].fd);
 				}
-			
+				return 0;
+			} else {
+				send_to_server(buff, sockfd);
+			}
+		}
 	}
 
 	return 0;
